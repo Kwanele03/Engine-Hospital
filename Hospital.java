dd817db19b7a3f547e566b2c4f293882bfb04893
Kwanele <kwanelentshele03@gmail.com>
2025-01-14 09:16:51 +0200

    Printing out manager's name and department

--- Hospital.java
+++ Hospital.java
@@ -9,7 +9,7 @@ public class Hospital {
         Doctor doctor = new Doctor(22003412, "Dr David Jones", "Health Dep and Operations.", "2021-02-17", 950, "David@2200");
         Nurses nurse = new Nurses(22175678, "Rebecca Nadioo","Labour and Operations Dep.","2022-02-10", 600, "Rebecca@2217");
     
-        
+        System.out.printf("My name is %s and I work in %s Derpartment \n", manager.getNmae(), manager.getDepartment());
 	}
 
 }
