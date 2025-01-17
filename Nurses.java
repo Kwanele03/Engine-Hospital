5dab7161a6119f88a739131deadd33fcfd3cd35c
Kwanele <112133974+Kwanele03@users.noreply.github.com>
2025-01-18 00:55:17 +0200

    Update Nurses.java

--- Nurses.java
+++ Nurses.java
@@ -11,5 +11,34 @@ public class Nurses extends Staff{
 		super(iD, fullName, derpartment,experience, salary, password );
 	
 	}
+	
+	
+	@Override
+	public void login() {
+		this.getID();
+		this.getPassword();
+       
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+        	
+           System.out.print("Enter Your ID: ");
+           int username = scanner.nextInt();
+           scanner.nextLine();
+           
+           System.out.print("Enter password: ");
+           String password = scanner.nextLine();
+
+           if (username == this.getID() && password.equals(this.getPassword())) {
+               System.out.println("Login successful!");
+               break;
+           } 
+           else {
+               System.out.println("Incorrect ID or password. Try again.");
+           }   
+       }
+
+   }
+	
 
 }
